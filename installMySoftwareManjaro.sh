6f88219dcac2ba16c192c18805a85f38ec10a925
ex <51292471+d43m0nhLInt3r@users.noreply.github.com>
2020-06-29 14:31:28 +0200

    added installMySoftwareManjaro.sh

---
+++ installMySoftwareManjaro.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+#Repositories
+pamac install dia firefox gimp inkscape kdenlive keepassxc krita neovim obs-studio signal-desktop torbrowser-launcher caffeine-ng etcher
+#AUR
+pamac build freetube-bin iridium-rpm vscodium
