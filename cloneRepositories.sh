24ebc13a15055e241c1e85b998ebefab6170e039
ex <51292471+d43m0nhLInt3r@users.noreply.github.com>
2020-07-22 00:09:33 +0200

    added cloneRepositories.sh

---
+++ cloneRepositories.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+if [ "$1" == "-h" ]; then
+	echo ""
+	echo " ========================================================= "
+	echo " \               cloneRepositories.sh  Script            / "
+	echo " \          Clone my Repositories with this script       / "
+	echo " \                Created by d43m0nhLInt3r               / "
+	echo " ========================================================= "
+	echo ""
+	echo "Usage: sudo ./cloneRepositories.sh"
+	echo "https://github.com/d43m0nhLInt3r/SimpleLinuxScripts"
+  exit 0
+fi
+cd && cd Documents
+git clone https://github.com/d43m0nhLInt3r/SimpleLinuxScripts & git clone https://github.com/d43m0nhLInt3r/Wallpaper & git clone https://github.com/d43m0nhLInt3r/dotfiles && echo "done" && exit 0
